3b38e7bef77bfa8ed39d4d48e2c569fa6b643f61
Yosef Awad <yosef.awad.1@gmail.com>
2022-04-16 22:29:14 +0300

    class20 done

--- script/script.js
+++ script/script.js
@@ -1,56 +1,31 @@
-let sum = 0, count = 0;
+let sum = 0, count = 0, stdArray = [];
 
-function saveToTable() {
 
+function readInput() {
     let nameTxt = document.getElementById("nameInput");
     let gradeTxt = document.getElementById("gradeInput");
-
-    let studentCount = document.getElementById("stdCnt");
-    let gradeAverage = document.getElementById("grdAvg");
-
     let missingTxt = document.getElementById("missing");
     let missingStr = "Invalid parameters: ";
-
-    let tableBody = document.getElementById("tableBody");
-
-    let validName = nameTxt.value == "" || nameTxt.value == null ? false : true;
+    let validName = (nameTxt.value == "" || nameTxt.value == null) || !validateName(nameTxt.value) ? false : true;
     let validGrade = gradeTxt.value == "" || gradeTxt.value == null ? false : true;
-
-    let tr = document.createElement("tr");
-    let td1 = document.createElement("td");
-    let td2 = document.createElement("td");
-    let label1 = document.createElement("label");
-    let label2 = document.createElement("label");
+    let stdObj;
 
     if (validGrade) {
         if (!(Number.parseInt(gradeTxt.value) >= 0 && Number.parseInt(gradeTxt.value) <= 100)) { validGrade = false }
-
     }
 
     if (validName && validGrade) {
-
-        sum += Number.parseInt(gradeTxt.value);
-        count++;
-
-        gradeAverage.textContent = Math.floor(sum / count);
-        studentCount.textContent = count;
-
         missingStr = "Invalid parameters: ";
         missingTxt.style.display = "none";
 
-        label1.textContent = nameTxt.value;
-        td1.appendChild(label1);
-        tr.appendChild(td1);
+        saveToTable(Number.parseInt(gradeTxt.value), nameTxt.value);
 
-        label2.textContent = gradeTxt.value;
-        td2.appendChild(label2);
-        tr.appendChild(td2);
-
-        tableBody.appendChild(tr);
+        stdObj = { stdName: nameTxt.value, stdGrade: Number.parseInt(gradeTxt.value) };
+        stdArray.push(stdObj);
+        saveToLocalStorage();
 
         nameTxt.value = "";
         gradeTxt.value = "";
-
     } else {
         if ((gradeTxt.value != "" && gradeTxt.value != null) && Number.parseInt(gradeTxt.value) >= 9000) missingStr = "Whaattt its over 9000!"
         else {
@@ -63,5 +38,57 @@ function saveToTable() {
         missingTxt.style.color = "red";
         missingTxt.style.display = "block";
     }
+}
+
+function saveToTable(grade, stdName) {
+
+    let studentCount = document.getElementById("stdCnt");
+    let gradeAverage = document.getElementById("grdAvg");
+
+    let tableBody = document.getElementById("tableBody");
+
+    let tr = document.createElement("tr");
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");
+    let label1 = document.createElement("label");
+    let label2 = document.createElement("label");
+
+    sum += grade;
+    count++;
+
+    gradeAverage.textContent = Math.floor(sum / count);
+    studentCount.textContent = count;
+
+
+
+    label1.textContent = stdName;
+    td1.appendChild(label1);
+    tr.appendChild(td1);
+
+    label2.textContent = grade;
+    td2.appendChild(label2);
+    tr.appendChild(td2);
+
+    tableBody.appendChild(tr);
 
 }
+
+function saveToLocalStorage() {
+    localStorage.setItem("studentsList", JSON.stringify(stdArray));
+}
+
+function readLocalStorage() {
+    if (localStorage.getItem("studentsList")) {
+        stdArray = JSON.parse(localStorage.getItem("studentsList"));
+        for (const i of stdArray) {
+            saveToTable(i.stdGrade, i.stdName);
+        }
+    }
+}
+
+function validateName(name) {
+    if (name.length < 5) return false;
+    return true;
+}
+
+readLocalStorage();
